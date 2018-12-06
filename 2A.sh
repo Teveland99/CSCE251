7adca01645700992e9710d6735c762d357da4c35
Teveland99 <35816852+Teveland99@users.noreply.github.com>
2018-12-05 22:02:17 -0600

    Update 2A.sh

--- 2A.sh
+++ 2A.sh
@@ -1,15 +1,1 @@
-function frequency() {
-  awk '
-     BEGIN { FS="[^a-zA-Z]+" } {
-         for (i=1; i<=NF; i++) {
-             word = tolower($i)
-             words[word]++
-         }
-     }
-     END {
-         for (w in words)
-              printf("%3d %s\n", words[w], w)
-     } ' | sort -rn
-}
-
-$ cat short-rockyou.txt | frequency | head -10
+tr -c '[:alnum:]' '[\n*]' < test.txt | sort | uniq -c | sort -nr | head  -25
