05286f005137a812d9b9d73d27be16567a1285fb
Teveland99 <35816852+Teveland99@users.noreply.github.com>
2018-12-06 22:42:45 -0600

    Update 2E.sh

--- 2E.sh
+++ 2E.sh
@@ -1,1 +1,8 @@
-
+low=$1
+high=$2
+if [ "$low" -lt "$high" ]
+then
+  awk -v low="$low"-v high="$high" '$1 >= low && $1 <= high { print substr($0, index($0, " ")+1) }' short-rockyou.txt
+else
+  echo "please enter the first argument as the smaller number and the second argument as the larger number"
+fi
