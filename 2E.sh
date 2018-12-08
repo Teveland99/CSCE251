aa0aeb691823b074c85fd0e64087c86b82fde1e1
Teveland99 <35816852+Teveland99@users.noreply.github.com>
2018-12-07 22:50:08 -0600

    Update 2E.sh

--- 2E.sh
+++ 2E.sh
@@ -1,8 +1,6 @@
-low=$1
-high=$2
-if [ "$low" -lt "$high" ]
+bottom=$1
+top=$2
+if [ "$bottom" -lt "$top" ]
 then
-  awk -v low="$low"-v high="$high" '$1 >= low && $1 <= high { print substr($0, index($0, " ")+1) }' short-rockyou.txt
-else
-  echo "please enter the first argument as the smaller number and the second argument as the larger number"
+  awk -v bottom="$bottom"-v top="$top" '$1 >= bottom && $1 <= top { print substr($0, index($0, " ")+1) }' short-rockyou.txt
 fi
