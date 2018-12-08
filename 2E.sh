336ef1bf6eefd74040a68ba03342506933409816
Teveland99 <35816852+Teveland99@users.noreply.github.com>
2018-12-07 22:50:46 -0600

    Update 2E.sh

--- 2E.sh
+++ 2E.sh
@@ -3,4 +3,6 @@ top=$2
 if [ "$bottom" -lt "$top" ]
 then
   awk -v bottom="$bottom"-v top="$top" '$1 >= bottom && $1 <= top { print substr($0, index($0, " ")+1) }' short-rockyou.txt
+else
+  echo "Please enter the first number to be lower than the second." 
 fi
