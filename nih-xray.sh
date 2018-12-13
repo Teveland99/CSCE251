777d547963e277da9a3d65cff23ad3fc46986c5d
Teveland99 <35816852+Teveland99@users.noreply.github.com>
2018-12-12 19:54:41 -0600

    Update nih-xray.sh

--- nih-xray.sh
+++ nih-xray.sh
@@ -1,4 +1,4 @@
-input="C:\Users\Tim\Documents\CSCE251\NIH_Xray_Data.csv"
+input="/home/misc/teveland/CSCE251/NIH_Xray_Data.csv"
 while IFS=',' read -r f1 f2 f3 f4 f5 f6 f7
 do 
   echo "$f1 $f2 $f3 $f4 $f5 $f6 $f7"
