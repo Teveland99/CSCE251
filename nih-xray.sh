f1ef37536eede8944e041d4d9d125776089c5881
Teveland99 <35816852+Teveland99@users.noreply.github.com>
2018-12-12 22:28:30 -0600

    Update nih-xray.sh

--- nih-xray.sh
+++ nih-xray.sh
@@ -1,5 +1,15 @@
-input="/home/misc/teveland/CSCE251/NIH_Xray_Data.csv"
-while IFS=',' read -r f1 f2 f3 f4 f5 f6 f7
-do 
+inputfile= "/home/misc/teveland/CSCE251/NIH_Xray_Data.csv"
+counter=0
+name = $1
+while IFS=',' read -r f1 f2 f3 f4 f5 f6 f7 
+do  
   echo "$f1 $f2 $f3 $f4 $f5 $f6 $f7"
-  done < "$input"
+  if
+    test[[$f2 == $name]]
+  then
+    counter=$((counter+1))
+    else
+  fi
+done < "$inputfile"
+fi
+echo "There are $counter xrays with $name label."
