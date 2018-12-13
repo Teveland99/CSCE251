14110d4888bb33b02f7b01aaee0d201ed401384b
Teveland99 <35816852+Teveland99@users.noreply.github.com>
2018-12-12 18:23:09 -0600

    Update 2C.sh

--- 2C.sh
+++ 2C.sh
@@ -1,1 +1,5 @@
-grep -o '\b\w\password\b' short-rockyou.txt
+input="C:\Users\Tim\Documents\CSCE251\NIH_Xray_Data.csv"
+while IFS=',' read -r f1 f2 f3 f4 f5 f6 f7
+do 
+  echo "$f1 $f2 $f3 $f4 $f5 $f6 $f7"
+  done < "$input"
